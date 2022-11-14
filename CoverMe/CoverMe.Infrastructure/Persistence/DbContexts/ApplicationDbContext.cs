d5aa37b653a2b3592b6b364d2c319ea1f15fb43e
R05 <mcrienstra@gmail.com>
2022-11-14 11:11:28 -0500

    ???

--- CoverMe/CoverMe.Infrastructure/Persistence/DbContexts/ApplicationDbContext.cs
+++ CoverMe/CoverMe.Infrastructure/Persistence/DbContexts/ApplicationDbContext.cs
@@ -28,6 +28,7 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser, Applicati
     public DbSet<Log> ServiceLog { get; set; }
     public DbSet<AuditLog> AuditLog { get; set; }
     public DbSet<ActivityLog> ActivityLog { get; set; }
+    public DbSet<Widget> Widget { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
