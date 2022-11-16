2841b2e2f5cc335976fb5d57b631c0a4973eaf9b
R05 <mcrienstra@gmail.com>
2022-11-16 10:54:35 -0500

    WUT

---
+++ CoverMe/CoverMe.Web/Controllers/pleaseconfirmController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoverMe.Web.Controllers;
+
+[AllowAnonymous]
+[ApiExplorerSettings(IgnoreApi = true)]
+
+
+
+    public class pleaseconfirmController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+
