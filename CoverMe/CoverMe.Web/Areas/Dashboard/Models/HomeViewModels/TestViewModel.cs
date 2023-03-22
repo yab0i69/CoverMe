22211adf2c19f879daf12ec6895e99f8aeac144a
R05 <mcrienstra@gmail.com>
2023-03-22 10:41:56 -0400

    WIP LOGS
    need help making the logs display, not sure if the info is even saving properly

--- CoverMe/CoverMe.Web/Areas/Dashboard/Models/HomeViewModels/TestViewModel.cs
+++ CoverMe/CoverMe.Web/Areas/Dashboard/Models/HomeViewModels/TestViewModel.cs
@@ -8,5 +8,6 @@
         public string Firstname { get; set; }
         public string Lastname { get; set; }
         public string date { get; set; }
-    }
+		public string data { get; set; }
+	}
 }
